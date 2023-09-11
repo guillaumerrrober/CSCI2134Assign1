ee2f72a63fd4bdc10a9ac7352272ae7efe8a1357
guillaumerrrober <guillaumerrrobert@gmail.com>
2023-09-11 08:41:37 -0300

    push code

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**
+ *CSCI 2134 - Assignment1,
+ * @author: Guillaume Robert, B00908938
+ * @description: Salary Calculation
+ * Understanding the basic of Git and Intellij
+ **/
+
+public class Hello {
+    public static void main(String[] args) {
+        String bannerNumber = "B00908938";
+        String csId= "unKnown";
+        String lastName = "Guillaume";
+        String firstName = "Robert";
+
+        System.out.printf("%s\n%s\n%s\n%s\n", bannerNumber, csId, lastName, firstName);
+        
+    }
+}
